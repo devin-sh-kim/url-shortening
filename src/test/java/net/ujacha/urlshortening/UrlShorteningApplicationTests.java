186a065a407b7e5e6ea5da5f9d6038cdf1cf5e17
Devin Sanghoon Kim <devin.sh.kim@gmail.com>
2021-03-10 21:26:32 +0900

    create project

---
+++ src/test/java/net/ujacha/urlshortening/UrlShorteningApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ujacha.urlshortening;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShorteningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
